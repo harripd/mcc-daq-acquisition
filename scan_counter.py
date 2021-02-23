c3c0440fd998c5328c0e5218061921610ae2e623
Philipp Klocke <Phil_K97@gmx.de>
2021-02-23 09:46:03 +0100

    Wrap code in main method

--- scan_counter.py
+++ scan_counter.py
@@ -10,137 +10,143 @@ import write_hdf5
 
 import time
 
-if os.name == "posix":
-    from counter_api_linux import CounterAPI
-elif os.name == "nt":
-    from counter_api_windows import CounterAPI
-else:
-    print("Operating System not supported!")
-    exit(0)
 
-try:
-    counterAPI = CounterAPI()
+def main():
 
-    counterAPI.setup()
-    buf = counterAPI.get_buf()
-    get_idx_fn = counterAPI.get_idx_fn
+    if os.name == "posix":
+        from counter_api_linux import CounterAPI
+    elif os.name == "nt":
+        from counter_api_windows import CounterAPI
+    else:
+        print("Operating System not supported!")
+        exit(0)
 
-    scanrate = counterAPI.start_scan()
-except:
-    # if we had an error we use mock data
+    try:
+        counterAPI = CounterAPI()
 
-    print("Exception while initializing Counter")
-    print("Press any key to continue with mock data...")
-    input()
+        counterAPI.setup()
+        buf = counterAPI.get_buf()
+        get_idx_fn = counterAPI.get_idx_fn
 
-    import threading
-    import numpy as np
-    
-    class MockCounter(threading.Thread):
-        def __init__(self):
-            threading.Thread.__init__(self)
-            self.buf = np.zeros(PLAIN_BUFFER_SIZE)
-            self.idx = 0
+        scanrate = counterAPI.start_scan()
+    except:
+        # if we had an error we use mock data
 
-        def run(self):
-            self.stop = False
-            while not self.stop:
-                sinargs = np.arange(self.idx, self.idx+SAMPLES_PER_BIN) * 2*np.pi / BUFFER_SIZE
-                sin = np.sin(sinargs) * 200
-                sin += (np.random.rand(SAMPLES_PER_BIN) - 0.5) * 20
-                buf[self.idx:self.idx+SAMPLES_PER_BIN*2:2] = (sin + 1000) / SAMPLES_PER_BIN
+        print("Exception while initializing Counter")
+        print("Press any key to continue with mock data...")
+        input()
 
-                noise = (np.random.rand(SAMPLES_PER_BIN) * 15 + 200)
+        import threading
 
-                buf[self.idx+1:self.idx+SAMPLES_PER_BIN*2+1:2] = (noise + 200) / SAMPLES_PER_BIN
+        class MockCounter(threading.Thread):
+            def __init__(self):
+                threading.Thread.__init__(self)
+                self.buf = np.zeros(PLAIN_BUFFER_SIZE)
+                self.idx = 0
 
-                self.idx = (self.idx + SAMPLES_PER_BIN*CHANNELS) % (PLAIN_BUFFER_SIZE)
-                time.sleep(1 / BIN_SIZE)
-        
-        def get_idx(self):
-            return self.idx
-        
-        def get_buf(self):
-            return self.buf
+            def run(self):
+                self.stop = False
+                while not self.stop:
+                    sinargs = np.arange(self.idx, self.idx+SAMPLES_PER_BIN) * 2*np.pi / BUFFER_SIZE
+                    sin = np.sin(sinargs) * 200
+                    sin += (np.random.rand(SAMPLES_PER_BIN) - 0.5) * 20
+                    buf[self.idx:self.idx+SAMPLES_PER_BIN*2:2] = (sin + 1000) / SAMPLES_PER_BIN
 
-    mock = MockCounter()
-    buf = mock.get_buf()
-    get_idx_fn = mock.get_idx
-    mock.start()
+                    noise = (np.random.rand(SAMPLES_PER_BIN) * 15 + 200)
 
+                    buf[self.idx+1:self.idx+SAMPLES_PER_BIN*2+1:2] = (noise + 200) / SAMPLES_PER_BIN
 
-# TODO: is it better to convert it into photon arrival times now or later?
+                    self.idx = (self.idx + SAMPLES_PER_BIN*CHANNELS) % (PLAIN_BUFFER_SIZE)
+                    time.sleep(1 / BIN_SIZE)
 
-processing_first_half = True
-midpoint = PLAIN_BUFFER_SIZE // 2
-current_time = 0
+            def get_idx(self):
+                return self.idx
 
-timestamps = []
-detectors = []
+            def get_buf(self):
+                return self.buf
 
-acquisition = False
-def toggle_acquisition():
-    global acquisition, timestamps, detectors, current_time
-    acquisition = not acquisition
-    print("Acquisition", "started" if acquisition else "stopped")
-    if not acquisition:
-        # User just turned off acquisition
-        if len(timestamps) == 0:
-            print("Nothing recorded permanently yet, not saving.")
-        else:
-            print("amount of timestamps:", current_time)
-            print("amount of photons:", len(detectors)//CHANNELS)
-            np_timestamps = np.array(timestamps) 
-            np_detectors = np.array(detectors)
-            timestamps_unit = 1/ACQUISITION_RATE 
-            write_hdf5.write_file(np_timestamps, np_detectors, timestamps_unit, fname=f'measurement_{int(time.time())}')
-
-        # reset measurement
-        timestamps = []
-        detectors = []
-        current_time = 0
+        mock = MockCounter()
+        buf = mock.get_buf()
+        get_idx_fn = mock.get_idx
+        mock.start()
 
 
-def update_callback_fn(buf, valid_idx):
-    global current_time, processing_first_half
+    # TODO: is it better to convert it into photon arrival times now or later?
 
-    if not acquisition:
-        return
+    processing_first_half = True
+    midpoint = PLAIN_BUFFER_SIZE // 2
+    current_time = 0
 
-    #print(f"update callback called, idx={valid_idx}")
-    if processing_first_half and valid_idx > midpoint:
-        for i in range(0, midpoint, CHANNELS):
-            if buf[i] > 1:
-                print(f"GREEN: Acquisition too coarse, {buf[i]} photons couldn't be distinguished")
-            if buf[i+1] > 1:
-                print(f"RED:   Acquisition too coarse, {buf[i+1]} photons couldn't be distinguished")
+    timestamps = []
+    detectors = []
 
-            for _ in range(buf[i]):
-                timestamps.append(current_time)
-                detectors.append(0)
-            for _ in range(buf[i+1]):
-                timestamps.append(current_time)
-                detectors.append(1)
-            current_time += 1
-        processing_first_half = False
-    if not processing_first_half and valid_idx < midpoint:
-        for i in range(midpoint, BUFFER_SIZE, CHANNELS):
-            if buf[i] > 1:
-                print(f"GREEN: Acquisition too coarse, {buf[i]} photons couldn't be distinguished")
-            if buf[i+1] > 1:
-                print(f"RED:   Acquisition too coarse, {buf[i+1]} photons couldn't be distinguished")
-            for _ in range(buf[i]):
-                timestamps.append(current_time)
-                detectors.append(0)
-            for _ in range(buf[i+1]):
-                timestamps.append(current_time)
-                detectors.append(1)
-            current_time += 1
-        processing_first_half = True
+    acquisition = False
+    def toggle_acquisition():
+        global acquisition, timestamps, detectors, current_time
+        acquisition = not acquisition
+        print("Acquisition", "started" if acquisition else "stopped")
+        if not acquisition:
+            # User just turned off acquisition
+            if len(timestamps) == 0:
+                print("Nothing recorded permanently yet, not saving.")
+            else:
+                print("amount of timestamps:", current_time)
+                print("amount of photons:", len(detectors)//CHANNELS)
+                np_timestamps = np.array(timestamps)
+                np_detectors = np.array(detectors)
+                timestamps_unit = 1/ACQUISITION_RATE
+                write_hdf5.write_file(np_timestamps, np_detectors, timestamps_unit, fname=f'measurement_{int(time.time())}')
 
-visualizer_backend.visualize(
-        buf,
-        get_idx_fn,
-        update_callback_fn,
-        keys=dict(space=toggle_acquisition))
-print("Ended Visualization")
+            # reset measurement
+            timestamps = []
+            detectors = []
+            current_time = 0
+
+
+    def update_callback_fn(buf, valid_idx):
+        global current_time, processing_first_half
+
+        if not acquisition:
+            return
+
+        #print(f"update callback called, idx={valid_idx}")
+        if processing_first_half and valid_idx > midpoint:
+            for i in range(0, midpoint, CHANNELS):
+                if buf[i] > 1:
+                    print(f"GREEN: Acquisition too coarse, {buf[i]} photons couldn't be distinguished")
+                if buf[i+1] > 1:
+                    print(f"RED:   Acquisition too coarse, {buf[i+1]} photons couldn't be distinguished")
+
+                for _ in range(buf[i]):
+                    timestamps.append(current_time)
+                    detectors.append(0)
+                for _ in range(buf[i+1]):
+                    timestamps.append(current_time)
+                    detectors.append(1)
+                current_time += 1
+            processing_first_half = False
+        if not processing_first_half and valid_idx < midpoint:
+            for i in range(midpoint, BUFFER_SIZE, CHANNELS):
+                if buf[i] > 1:
+                    print(f"GREEN: Acquisition too coarse, {buf[i]} photons couldn't be distinguished")
+                if buf[i+1] > 1:
+                    print(f"RED:   Acquisition too coarse, {buf[i+1]} photons couldn't be distinguished")
+                for _ in range(buf[i]):
+                    timestamps.append(current_time)
+                    detectors.append(0)
+                for _ in range(buf[i+1]):
+                    timestamps.append(current_time)
+                    detectors.append(1)
+                current_time += 1
+            processing_first_half = True
+
+    visualizer_backend.visualize(
+            buf,
+            get_idx_fn,
+            update_callback_fn,
+            keys=dict(space=toggle_acquisition))
+    print("Ended Visualization")
+
+
+if __name__ == "__main__":
+    main()
